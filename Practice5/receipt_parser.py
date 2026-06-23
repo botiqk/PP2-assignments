e583406826e63ba6df2567b0422e079d047f9343
Your Name <your_email@whatever.comclear>
2026-06-23 10:52:34 +0500

    Add Practice5 - Python RegEx and receipt parsing examples

--- Practice5/receipt_parser.py
+++ Practice5/receipt_parser.py
@@ -1,30 +1,53 @@
 import re
 import json
 
+#read a file
 with open("raw.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
-dt = re.search(r"Время:\s*(\d{2}\.\d{2}\.\d{4})\s+(\d{2}:\d{2}:\d{2})", text)
-date = dt.group(1) if dt else None
-time = dt.group(2) if dt else None
+#extracting all prices
+prices = re.findall(r"\d+\s?\d*,\d{2}", text)
 
-payment = re.search(r"Банковская карта|Наличные", text)
-payment_method = payment.group() if payment else None
+#remove spaces in numbers
+prices = [p.replace(" ", "") for p in prices]
 
+#extracting names of products
 products = re.findall(r"\d+\.\n(.+)", text)
 
-prices = re.findall(r"Стоимость\n([\d\s]+,\d{2})", text)
-prices = [float(p.replace(" ", "").replace(",", ".")) for p in prices]
+#extracting dates and time
+datetime_match = re.search(r"\d{2}\.\d{2}\.\d{4}\s\d{2}:\d{2}:\d{2}", text)
 
-total_amount = sum(prices)
+date = None
+time = None
 
-result = {
-    "date": date,
-    "time": time,
-    "payment_method": payment_method,
+if datetime_match:
+    dt = datetime_match.group()
+    date, time = dt.split()
+
+#determine payment method
+payment = re.search(r"Банковская карта|Наличные|Карта", text)
+
+payment_method = payment.group() if payment else None
+
+#get overall sum from ticket
+total_match = re.search(r"ИТОГО:\n([\d\s,]+)", text)
+
+total = None
+if total_match:
+    total = total_match.group(1).replace(" ", "")
+
+#determine general sum(by products price)
+total_calc = sum(float(p.replace(",", ".")) for p in prices)
+
+#form data structures(json)
+data = {
     "products": products,
     "prices": prices,
-    "total_amount": total_amount
+    "total": total_calc,
+    "date": date,
+    "time": time,
+    "payment_method": payment_method
 }
 
-print(json.dumps(result, ensure_ascii=False, indent=4))
+#res (python -> json)
+print(json.dumps(data, indent=4, ensure_ascii=False))
