18f16044087f7442bad2824fe9aadc17c9aa0517
Your Name <your_email@whatever.comclear>
2026-06-25 14:37:04 +0500

    Add Practice6 - file handling and built-in functions examples

--- Practice6/builtin_functions/enumerate_zip_examples.py
+++ Practice6/builtin_functions/enumerate_zip_examples.py
@@ -10,18 +10,3 @@ scores = [67, 86, 73]
 
 for name, score in zip(names, scores):
     print(name, score)
-
-#type checking
-
-x = "123"
-
-print(type(x))
-
-number = int(x)
-
-print(type(number))
-
-y = 12.5
-
-print(int(y))
-print(str(y))
